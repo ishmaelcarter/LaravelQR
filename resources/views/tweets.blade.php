be200be81acbbe03e75e5c40ad258f1c3fb39123
Ishmael Carter <icarter@bvtravel.com>
2019-04-01 15:48:57 -0700

    trending page

--- resources/views/tweets.blade.php
+++ resources/views/tweets.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="tweet-media">
   @foreach ($trendingUnique as $trending)
-    @if ($loop->iteration < 20)
+    @if ($loop->iteration > 40 && $loop->iteration < 80)
       <div class="tweet-inner">
         <div style="background-image: url('{{$trending->media}}')"><a class="" href="{{$trending->url}}">{{$trending->text}}</a></div>
       </div>
