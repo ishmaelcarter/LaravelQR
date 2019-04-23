24eb594e8b61f7771ebe5519c434251b3ab258f4
Ishmael Carter <icarter@bvtravel.com>
2019-04-23 14:00:52 -0700

    Adding navbar link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,9 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="/trending">Trending</a>
+                            </li>
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
