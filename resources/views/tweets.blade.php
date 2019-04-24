e9cb6545022aef0da2665342670d087212909193
Ishmael Carter <icarter@bvtravel.com>
2019-04-24 15:30:46 -0700

    Removing tweet text

--- resources/views/tweets.blade.php
+++ resources/views/tweets.blade.php
@@ -5,7 +5,7 @@
   @foreach ($trendingUnique as $tweet)
     @if ($loop->iteration < 30)
       <div class="tweet-inner">
-        <a class="" href="{{$tweet->url}}"><div style="background-image: url('{{$tweet->media}}')">{{$tweet->text}}</div></a>
+        <a class="" href="{{$tweet->url}}" alt="{{$tweet->text}}"><div style="background-image: url('{{$tweet->media}}')"></div></a>
       </div>
     @endif
   @endforeach
